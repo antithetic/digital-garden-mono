f7445204772e2f8d1a1c033ff178e0ecf4bab123
Raúl Garcia <ego.war@icloud.com>
2025-06-15 01:52:13 -0700

    fix(eslint): sanity eslint config updated

--- apps/garden-studio/sanity.config.ts
+++ apps/garden-studio/sanity.config.ts
@@ -1,7 +1,8 @@
+import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+
+import {schemaTypes} from './src/schemaTypes'
 
 export default defineConfig({
   name: 'default',
---
+++ apps/garden-studio/src/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+export const schemaTypes = [
+  // Documents
+  // Objects
+  // Assets
+]
