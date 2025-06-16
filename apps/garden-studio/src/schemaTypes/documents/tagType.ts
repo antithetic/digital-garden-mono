e214a68cfa676e8896304f2be3b5cbd8c9b9c0fd
Raúl Garcia <ego.war@icloud.com>
2025-06-15 23:15:27 -0700

    feat(tags): added icons to tag and updated preview
    ]\

--- apps/garden-studio/src/schemaTypes/documents/tagType.ts
+++ apps/garden-studio/src/schemaTypes/documents/tagType.ts
@@ -1,5 +1,6 @@
 import {Tag} from 'lucide-react'
 import {defineArrayMember, defineField, defineType} from 'sanity'
+import {preview} from 'sanity-plugin-icon-picker'
 
 export const tagType = defineType({
   name: 'tag',
@@ -28,6 +29,12 @@ export const tagType = defineType({
       rows: 2,
     }),
     defineField({
+      name: 'icon',
+      title: 'Icon',
+      description: 'A u icon for this tag',
+      type: 'iconPicker',
+    }),
+    defineField({
       name: 'color',
       title: 'Tag Color',
       description: 'A unique color for this tag',
@@ -49,8 +56,16 @@ export const tagType = defineType({
   ],
   preview: {
     select: {
-      title: 'name',
-      subtitle: 'description',
+      name: 'name',
+      description: 'description',
+      icon: 'icon',
+    },
+    prepare({name, description, icon}) {
+      return {
+        title: name,
+        subtitle: description,
+        media: preview(icon),
+      }
     },
   },
 })
